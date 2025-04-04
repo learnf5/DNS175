4fb96fc98617c7f372c39c77e6e369e5d9f09429
learnf5 <99698691+learnf5@users.noreply.github.com>
2025-04-04 10:30:09 -0400

    Create 20-DNS175-C2B05-IQUERY.sh

---
+++ 20-DNS175-C2B05-IQUERY.sh
@@ -0,0 +1,12 @@
+# download archive files from github, copy to bigip1 and run load/merge
+archive=create_dns_listener.scf
+curl --silent https://raw.githubusercontent.com/learnf5/dns/main/$archive             --output /tmp/$archive
+curl --silent https://raw.githubusercontent.com/learnf5/admin/main/train_base.scf     --output /tmp/train_base.scf
+curl --silent https://raw.githubusercontent.com/learnf5/admin/main/train_base_171.ucs --output /tmp/train_base.ucs
+sudo scp /tmp/*.scf          192.168.1.31:/var/local/scf/
+sudo scp /tmp/train_base.ucs 192.168.1.31:/var/local/ucs/
+sudo ssh 192.168.1.31 tmsh load sys config merge file $archive
+
+# confirm bigip1 is active again 
+sleep 5
+for i in {1..30}; do [ "$(sudo ssh root@192.168.1.31 cat /var/prompt/ps1)" = "Active" ] && break; sleep 5; done

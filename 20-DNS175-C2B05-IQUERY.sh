ae2cb32b342a440518967b9359ba3492aaf527bb
learnf5 <99698691+learnf5@users.noreply.github.com>
2025-06-12 11:50:13 -0400

    remove 2nd bigip1 active check

--- 20-DNS175-C2B05-IQUERY.sh
+++ 20-DNS175-C2B05-IQUERY.sh
@@ -6,7 +6,3 @@ curl --silent https://raw.githubusercontent.com/learnf5/$COURSE_ID/main/train_ba
 sudo scp /tmp/*.scf          192.168.1.31:/var/local/scf/
 sudo scp /tmp/train_base.ucs 192.168.1.31:/var/local/ucs/
 sudo ssh 192.168.1.31 tmsh load sys config merge file $archive
-
-# confirm bigip1 is active again 
-sleep 5
-for i in {1..30}; do [ "$(sudo ssh root@192.168.1.31 cat /var/prompt/ps1)" = "Active" ] && break; sleep 5; done

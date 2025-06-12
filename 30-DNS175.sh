571e2213b613105ad9a8def0c1357f6867d504fa
learnf5 <99698691+learnf5@users.noreply.github.com>
2025-06-12 11:44:21 -0400

    add bigip2 active check for SYNC lab

--- 30-DNS175.sh
+++ 30-DNS175.sh
@@ -1,3 +1,6 @@
 # confirm bigip1 is active again 
 sleep 5
 for i in {1..30}; do [ "$(sudo ssh root@192.168.1.31 cat /var/prompt/ps1)" = "Active" ] && break; sleep 5; done
+
+# confirm bigip2 is active again, if this is the SYNC lab
+[[ "$LAB_NUMBER" =~ "-SYNC" ]] && for i in {1..30}; do [ "$(sudo ssh root@192.168.2.31 cat /var/prompt/ps1)" = "Active" ] && break; sleep 5; done

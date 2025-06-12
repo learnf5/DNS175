45c033d23a1ec4b312c5b544b505c7b2008bea2e
learnf5 <99698691+learnf5@users.noreply.github.com>
2025-06-12 11:38:55 -0400

    s/dns|admin/$COURSE_ID/

--- 20-DNS175-C3D06-MONITOR.sh
+++ 20-DNS175-C3D06-MONITOR.sh
@@ -1,8 +1,8 @@
 # download archive files from github, copy to bigip1 and run load/merge
 archive=test_topology_lb.scf
-curl --silent https://raw.githubusercontent.com/learnf5/dns/main/$archive             --output /tmp/$archive
-curl --silent https://raw.githubusercontent.com/learnf5/admin/main/train_base.scf     --output /tmp/train_base.scf
-curl --silent https://raw.githubusercontent.com/learnf5/admin/main/train_base_171.ucs --output /tmp/train_base.ucs
+curl --silent https://raw.githubusercontent.com/learnf5/$COURSE_ID/main/$archive             --output /tmp/$archive
+curl --silent https://raw.githubusercontent.com/learnf5/$COURSE_ID/main/train_base.scf     --output /tmp/train_base.scf
+curl --silent https://raw.githubusercontent.com/learnf5/$COURSE_ID/main/train_base_171.ucs --output /tmp/train_base.ucs
 sudo scp /tmp/*.scf          192.168.1.31:/var/local/scf/
 sudo scp /tmp/train_base.ucs 192.168.1.31:/var/local/ucs/
 sudo ssh 192.168.1.31 tmsh load sys config merge file $archive

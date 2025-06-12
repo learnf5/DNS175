69b48899105d36d3e7f41d1af94fc00ab1e4bc24
learnf5 <99698691+learnf5@users.noreply.github.com>
2025-06-12 11:46:08 -0400

    remove bigip2 active checks
    now included in 10- and 30-DNS175.sh

--- 20-DNS175-C3E07-SYNC.sh
+++ 20-DNS175-C3E07-SYNC.sh
@@ -7,15 +7,8 @@ sudo scp /tmp/*.scf          192.168.1.31:/var/local/scf/
 sudo scp /tmp/train_base.ucs 192.168.1.31:/var/local/ucs/
 sudo ssh 192.168.1.31 tmsh load sys config merge file $archive
 
-# confirm bigip2 is active 
-for i in {1..30}; do [ "$(sudo ssh root@192.168.2.31 cat /var/prompt/ps1)" = "Active" ] && break; sleep 5; done
-
 # download archive files from github, copy to bigip2 and run load/merge
 archive2=base_system_prep.scf
 curl --silent https://raw.githubusercontent.com/learnf5/$COURSE_ID/main/$archive2     --output /tmp/$archive2
 sudo scp /tmp/$archive2          192.168.2.31:/var/local/scf/
 sudo ssh 192.168.2.31 tmsh load sys config merge file $archive2
-
-# confirm bigip2 is active again 
-sleep 5
-for i in {1..30}; do [ "$(sudo ssh root@192.168.2.31 cat /var/prompt/ps1)" = "Active" ] && break; sleep 5; done
